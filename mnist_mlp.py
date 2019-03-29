b49b127b95166da54debd8419b0dad56584b1084
jenslee <913470452@qq.com>
2019-03-29 15:17:07 +0800

    keras_mnist_simple_try

---
+++ mnist_mlp.py
@@ -0,0 +1,50 @@
+import keras
+import cv2
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+import numpy as np
+from keras.optimizers import RMSprop
+from keras.datasets import mnist
+
+batch_size = 128
+num_classes = 10
+epochs = 20
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784)
+x_test = x_test.reshape(10000, 784)
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+print(x_train.shape)
+print(x_test.shape)
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = Sequential()
+model.add(Dense(512, activation='relu', input_shape=(784,)))
+model.add(Dropout(0.2))
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer=RMSprop(),
+              metrics=['accuracy'])
+
+history = model.fit(x_train, y_train,
+                    batch_size=batch_size,
+                    epochs=epochs,
+                    verbose=1,
+                    validation_data=(x_test, y_test))
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test score : ', score[0])
+print('test accuracy : ', score[1])
